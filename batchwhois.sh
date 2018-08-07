66a1bbbd21e6caffcc9b96a9fdbb06fbd2d2fbd9
Ecksy <38439556+Ecksy@users.noreply.github.com>
2018-08-07 15:55:17 -0500

    Update batchwhois.sh

--- batchwhois.sh
+++ batchwhois.sh
@@ -4,5 +4,8 @@ while read -r line
 do
     echo "*********************************************************************" | tee -a client-whois.txt
     echo "*********************************************************************" | tee -a client-whois.txt
+    echo "" | tee -a client-whois.txt
+    echo "WHOIS query for:" $line | tee -a client-whois.txt
     whois -H $line | tee -a client-whois.txt
+    echo "" | tee -a client-whois.txt
 done < "$filename"
