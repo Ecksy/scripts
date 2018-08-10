322bf6529879fd1386e87a13efd0a151ebd14cc5
Ecksy <38439556+Ecksy@users.noreply.github.com>
2018-08-10 12:26:04 -0500

    Changed some text prompts

--- nmap-ip-port-list-script.sh
+++ nmap-ip-port-list-script.sh
@@ -1,17 +1,20 @@
 #!/usr/bin/bash
 echo ""
-echo "********** Listing Directory Contents for gnmap extension **********"
+echo "********** Here are your gnmap files **********"
 echo ""
 ls *.gnmap
 echo ""
-echo "********** Select file to process **********"
-read -p "Enter filename: " filename
+echo "********** Select file(s) to process **********"
+read -p "Enter filename(s): " filename
 echo ""
 cat $filename | grep open | awk '{printf "%s\t", $2;
  for (i=4;i<=NF;i++) {
  split($i,a,"/");
  if (a[2]=="open") printf ",%s",a[1];}
 print ""}' | sed -e 's/,//' | tee -a client-nmap-port-list.txt
+echo ""
+echo "!!!!! Wrote results to file client-nmap-port-list.txt !!!!!"
+echo ""
 echo ""
 
 #one-liner
