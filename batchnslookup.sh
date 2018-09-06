b28c49d58b9bc8a8e1d369ed1fed5722b41c1cc5
Ecksy <38439556+Ecksy@users.noreply.github.com>
2018-09-06 15:45:08 -0500

    Add files via upload

---
+++ batchnslookup.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+filename="$1"
+while read -r line
+do
+    echo "***********************************************************************************************" | tee -a client-nslookup.txt
+    echo "***********************************************************************************************" | tee -a client-nslookup.txt
+    echo "" | tee -a client-nslookup.txt
+    echo "nslookup query for:" $line | tee -a client-nslookup.txt
+    nslookup $line | tee -a client-nslookup.txt
+    echo "" | tee -a client-nslookup.txt
+done < "$filename"
