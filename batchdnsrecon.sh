e7f8e31e3302f916fb42cc1948ef5322f089d79d
Ecksy <38439556+Ecksy@users.noreply.github.com>
2018-09-24 10:36:22 -0500

    Changed output filename, added spacing between queries

--- batchdnsrecon.sh
+++ batchdnsrecon.sh
@@ -2,5 +2,9 @@
 filename="$1"
 while read -r line
 do
-    dnsrecon -d $line >> dnsrecon.out.txt
+    echo "*********************************************************************" | tee -a client-dnsrecon.txt
+    echo "*********************************************************************" | tee -a client-dnsrecon.txt
+    echo "" | tee -a client-dnsrecon.txt
+    dnsrecon -d $line | tee -a client-dnsrecon.txt
+    echo "" | tee -a client-dnsrecon.txt
 done < "$filename"
