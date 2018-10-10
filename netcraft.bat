24ba50e39685cbf9997ce382308c5910e460f0e1
Ecksy <38439556+Ecksy@users.noreply.github.com>
2018-10-10 10:26:30 -0500

    Update netcraft.bat

--- netcraft.bat
+++ netcraft.bat
@@ -5,7 +5,6 @@ echo ---------------------------------
 echo **** Netcraft Domain Check ****
 SET /p domain="Enter domain name to check: "
 START  "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" https://toolbar.netcraft.com/site_report?url=%domain%^&refresh=1#history_table
-#START  "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" https://www.shodan.io/search^?query=%ip_addr%
 set choice=
 set /p choice="Do you want to restart? Press 'e' for Yes, 'Enter' to quit:"
 if not '%choice%'=='' set choice=%choice:~0,1%
