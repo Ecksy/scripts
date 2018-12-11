6c3e4f365e633c91fcf183a015b7d742bcc8d418
Ecksy <38439556+Ecksy@users.noreply.github.com>
2018-12-11 10:40:14 -0600

    Update splitter.sh

--- splitter.sh
+++ splitter.sh
@@ -5,7 +5,7 @@
 #Count number of files
 if [ $# -eq 0 ]
 then
-	echo "Split nmap.gnmap output for common open ports 25,80,139,443,8080,9100"
+	echo "Split nmap.gnmap output for common open ports 25,80,139,443,8080"
 	echo "USAGE: splitter.sh <grepable nmap files>"
 	exit 1;
 fi
@@ -21,12 +21,18 @@ do
 	echo $outname
 	echo "$COUNTER: Scanning $line"
 	grep "25/open" $name  | awk '{print $2}'>>25-$outname.txt
-	grep "80/open" $name | awk '{print $2}'>>80-$outname.txt
+	grep "135/open" $name | awk '{print $2}'>> 135-$outname.txt
+	grep "1434/open" $name | awk '{print $2}'>> 1434-$outname.txt
+	grep "137/open" $name | awk '{print $2}'>> 137-$outname.txt
+	grep "138/open" $name | awk '{print $2}'>> 138-$outname.txt
+	grep "1433/open" $name | awk '{print $2}'>> 1433-$outname.txt
+	grep "23/open" $name | awk '{print $2}'>> 23-$outname.txt
+	grep "53/open" $name | awk '{print $2}'>> 53-$outname.txt
+	grep "80/open" $name | awk '{print $2}'>> 80-$outname.txt
 	grep "139/open" $name  | awk '{print $2}'>>139-$outname.txt
 	grep "443/open" $name  | awk '{print $2}'>>443-$outname.txt
 	grep "8080/open" $name  | awk '{print $2}'>>8080-$outname.txt
 	grep "9001/open" $name  | awk '{print $2}'>>9001-$outname.txt
-	grep "9100/open" $name  | awk '{print $2}'>>9100-$outname.txt
-#
+	grep "/open" $name  | awk '{print $2}'>>all-$outname.txt
 	echo "--------------------------------------"
 done 
