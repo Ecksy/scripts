13ae7de052db315f569cf3366f1b24d58ecce57f
Ecksy <38439556+Ecksy@users.noreply.github.com>
2018-12-19 13:26:03 -0600

    Update shodan_ip.py

--- shodan_ip.py
+++ shodan_ip.py
@@ -1,6 +1,4 @@
-#!/usr/bin/env python2
-# I found this script on GH but it dies when the query limit is reached, had other bugs too in the original
-# Shodan limits queries to 1 per second. Added a 1 second sleep to each query to prevent errors
+#!/usr/bin/env python
 
 import shodan
 import sys
@@ -9,22 +7,43 @@ import netaddr
 import time
 
 
-KEY = 'add your api key here'
+KEY = 'YOUR-API-KEY-HERE'
 api = shodan.Shodan(KEY)
 
 
 def lookup_ip(ip):
+    print('')
     print(ip)
-    print('=' * len(ip))
+    print('Querying Shodan...')
+    print('=' * len(ip) * 3)
     time.sleep(1)
 
     try:
         host = api.host(ip)
-	print('Org: {0}'.format(host.get('org', '')))
-        print('Operating System: {0}'.format(host.get('os', '')))
-        print('Hostnames: {0}'.format(', '.join(host.get('hostnames', []))))
-        print('Ports: {0}'.format(', '.join([str(p) for p in host['ports']])))
-        print('Vulns: {0}'.format(', '.join(host.get('vulns', []))))
+#	print('')
+	print('Org		: {0}'.format(host.get('org', '')))
+	print('City		: {0}'.format(host.get('city', '')))
+	print('State/Region	: {0}'.format(host.get('region_code', '')))
+	print('Country		: {0}'.format(host.get('country_name', '')))
+	print('Country Code	: {0}'.format(host.get('country_code', '')))
+	print('Postal		: {0}'.format(host.get('postal_code', '')))
+	print('Area Code	: {0}'.format(host.get('area_code', '')))
+	print('Lat		: {0}'.format(host.get('latitude', '')))
+	print('Long		: {0}'.format(host.get('longitude', '')))
+	print('=' * len(ip) * 3)
+	print('ISP		: {0}'.format(host.get('isp', '')))
+        print('Hostname	: {0}'.format(', '.join(host.get('hostnames', []))))
+	print('Reported IP	: {0}'.format(host.get('ip_str', '')))
+        print('OS		: {0}'.format(host.get('os', '')))
+	print('ASN		: {0}'.format(host.get('asn', '')))
+        print('Ports		: {0}'.format(', '.join([str(p) for p in host['ports']])))
+	print('Tags		: {0}'.format(host.get('tags', '')))
+        print('Vulns		: {0}'.format(', '.join(host.get('vulns', []))))
+	print('Banner		: {0}'.format(host.get('banner', '')))
+	print('Device		: {0}'.format(host.get('devicetype', '')))
+	print('Domain		: {0}'.format(host.get('domains', '')))
+	print('Net		: {0}'.format(host.get('net', '')))
+	print('Uptime		: {0}'.format(host.get('uptime', '')))
         print('')
 
     except shodan.APIError as e:
@@ -40,6 +59,9 @@ IP, a range of IPs, or in CIDR notation.
 """
     parser = argparse.ArgumentParser(description=desc)
     ipgroup = parser.add_mutually_exclusive_group(required=True)
+#    ipgroup.add_argument('-l', action='store', default=None,
+#                         metavar="filename",
+#                         help='List of IP addresses, one per line')
     ipgroup.add_argument('-i', action='store', default=None,
                          metavar="IP",
                          help='A single IP address. ex: 192.168.1.1')
