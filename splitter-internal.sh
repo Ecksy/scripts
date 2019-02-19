32429f1e804b4fef9b39acc3a6068f048229ecf2
Ecksy <38439556+Ecksy@users.noreply.github.com>
2019-02-19 14:35:11 -0600

    Update splitter-internal.sh

--- splitter-internal.sh
+++ splitter-internal.sh
@@ -5,7 +5,7 @@
 #Count number of files
 if [ $# -eq 0 ]
 then
-	echo "Split nmap.gnmap output for common open ports 25,80,139,443,8080"
+	echo "Split nmap.gnmap output for common open internal ports 25,80,139,443,8080"
 	echo "USAGE: splitter.sh <grepable nmap files>"
 	exit 1;
 fi
@@ -20,19 +20,19 @@ do
 #	outname=`echo $line |sed -e 's;/;-;'`
 	echo $outname
 	echo "$COUNTER: Scanning $line"
-	grep "25/open" $name  | awk '{print $2}'>> 25-$outname.txt
-	grep "135/open" $name | awk '{print $2}'>> 135-$outname.txt
-	grep "1434/open" $name | awk '{print $2}'>> 1434-$outname.txt
-	grep "137/open" $name | awk '{print $2}'>> 137-$outname.txt
-	grep "138/open" $name | awk '{print $2}'>> 138-$outname.txt
-	grep "1433/open" $name | awk '{print $2}'>> 1433-$outname.txt
-	grep "23/open" $name | awk '{print $2}'>> 23-$outname.txt
-	grep "53/open" $name | awk '{print $2}'>> 53-$outname.txt
-	grep "80/open" $name | awk '{print $2}'>> 80-$outname.txt
-	grep "139/open" $name  | awk '{print $2}'>> 139-$outname.txt
-	grep "443/open" $name  | awk '{print $2}'>> 443-$outname.txt
-	grep "8080/open" $name  | awk '{print $2}'>> 8080-$outname.txt
-	grep "9001/open" $name  | awk '{print $2}'>> 9001-$outname.txt
-	grep "/open" $name  | awk '{print $2}' | sort -t. -k1,1n -k2.2n -k3,3n -k4,4n >> all-$outname.txt
+	grep "25/open" $name  | awk '{print $2}' | sort -t. -k1,1n -k2,2n -k3,3n -k4,4n >> 25-$outname.txt
+	grep "135/open" $name | awk '{print $2}' | sort -t. -k1,1n -k2.2n -k3,3n -k4,4n >> 135-$outname.txt
+	grep "1434/open" $name | awk '{print $2}' | sort -t. -k1,1n -k2.2n -k3,3n -k4,4n >> 1434-$outname.txt
+	grep "137/open" $name | awk '{print $2}' | sort -t. -k1,1n -k2.2n -k3,3n -k4,4n >> 137-$outname.txt
+	grep "138/open" $name | awk '{print $2}' | sort -t. -k1,1n -k2.2n -k3,3n -k4,4n >> 138-$outname.txt
+	grep "1433/open" $name | awk '{print $2}' | sort -t. -k1,1n -k2.2n -k3,3n -k4,4n >> 1433-$outname.txt
+	grep "23/open" $name | awk '{print $2}' | sort -t. -k1,1n -k2.2n -k3,3n -k4,4n >> 23-$outname.txt
+	grep "53/open" $name | awk '{print $2}' | sort -t. -k1,1n -k2.2n -k3,3n -k4,4n >> 53-$outname.txt
+	grep "80/open" $name | awk '{print $2}' | sort -t. -k1,1n -k2.2n -k3,3n -k4,4n >> 80-$outname.txt
+	grep "139/open" $name  | awk '{print $2}' | sort -t. -k1,1n -k2.2n -k3,3n -k4,4n >> 139-$outname.txt
+	grep "443/open" $name  | awk '{print $2}' | sort -t. -k1,1n -k2.2n -k3,3n -k4,4n >> 443-$outname.txt
+	grep "8080/open" $name  | awk '{print $2}' | sort -t. -k1,1n -k2.2n -k3,3n -k4,4n >> 8080-$outname.txt
+	grep "9001/open" $name  | awk '{print $2}' | sort -t. -k1,1n -k2.2n -k3,3n -k4,4n >> 9001-$outname.txt
+	grep "/open" $name  | awk '{print $2}' | sort -t. -k1,1n -k2,2n -k3,3n -k4,4n >> all-$outname.txt
 	echo "--------------------------------------"
 done 
