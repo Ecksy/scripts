f1841ec3fad1ed049958580b78dcef0aa62371a8
Ecksy <38439556+Ecksy@users.noreply.github.com>
2019-03-13 09:55:34 -0500

    Update splitter-internal.sh

--- splitter-internal.sh
+++ splitter-internal.sh
@@ -20,19 +20,23 @@ do
 #	outname=`echo $line |sed -e 's;/;-;'`
 	echo $outname
 	echo "$COUNTER: Scanning $line"
-	grep "25/open" $name  | awk '{print $2}' | sort -t. -k1,1n -k2,2n -k3,3n -k4,4n >> 25-$outname.txt
-	grep "135/open" $name | awk '{print $2}' | sort -t. -k1,1n -k2.2n -k3,3n -k4,4n >> 135-$outname.txt
-	grep "1434/open" $name | awk '{print $2}' | sort -t. -k1,1n -k2.2n -k3,3n -k4,4n >> 1434-$outname.txt
-	grep "137/open" $name | awk '{print $2}' | sort -t. -k1,1n -k2.2n -k3,3n -k4,4n >> 137-$outname.txt
-	grep "138/open" $name | awk '{print $2}' | sort -t. -k1,1n -k2.2n -k3,3n -k4,4n >> 138-$outname.txt
-	grep "1433/open" $name | awk '{print $2}' | sort -t. -k1,1n -k2.2n -k3,3n -k4,4n >> 1433-$outname.txt
-	grep "23/open" $name | awk '{print $2}' | sort -t. -k1,1n -k2.2n -k3,3n -k4,4n >> 23-$outname.txt
-	grep "53/open" $name | awk '{print $2}' | sort -t. -k1,1n -k2.2n -k3,3n -k4,4n >> 53-$outname.txt
-	grep "80/open" $name | awk '{print $2}' | sort -t. -k1,1n -k2.2n -k3,3n -k4,4n >> 80-$outname.txt
-	grep "139/open" $name  | awk '{print $2}' | sort -t. -k1,1n -k2.2n -k3,3n -k4,4n >> 139-$outname.txt
-	grep "443/open" $name  | awk '{print $2}' | sort -t. -k1,1n -k2.2n -k3,3n -k4,4n >> 443-$outname.txt
-	grep "8080/open" $name  | awk '{print $2}' | sort -t. -k1,1n -k2.2n -k3,3n -k4,4n >> 8080-$outname.txt
+	grep "25/open" $name  | awk '{print $2}' | sort -t. -k1,1n -k2,2n -k3,3n -k4,4n >> 25-smtp-$outname.txt
+	grep "88/open" $name  | awk '{print $2}' | sort -t. -k1,1n -k2,2n -k3,3n -k4,4n >> 88-kerberos-$outname.txt
+	grep "135/open" $name | awk '{print $2}' | sort -t. -k1,1n -k2.2n -k3,3n -k4,4n >> 135-rpc-$outname.txt
+	grep "1434/open" $name | awk '{print $2}' | sort -t. -k1,1n -k2.2n -k3,3n -k4,4n >> 1434-sql-$outname.txt
+	grep "137/open" $name | awk '{print $2}' | sort -t. -k1,1n -k2.2n -k3,3n -k4,4n >> 137-nbns-$outname.txt
+	grep "138/open" $name | awk '{print $2}' | sort -t. -k1,1n -k2.2n -k3,3n -k4,4n >> 138-nbds-$outname.txt
+	grep "1433/open" $name | awk '{print $2}' | sort -t. -k1,1n -k2.2n -k3,3n -k4,4n >> 1433-sql-$outname.txt
+	grep "23/open" $name | awk '{print $2}' | sort -t. -k1,1n -k2.2n -k3,3n -k4,4n >> 23-telnet-$outname.txt
+	grep "53/open" $name | awk '{print $2}' | sort -t. -k1,1n -k2.2n -k3,3n -k4,4n >> 53-dns-$outname.txt
+	grep "80/open" $name | awk '{print $2}' | sort -t. -k1,1n -k2.2n -k3,3n -k4,4n >> 80-http-$outname.txt
+	grep "139/open" $name  | awk '{print $2}' | sort -t. -k1,1n -k2.2n -k3,3n -k4,4n >> 139-nbss-$outname.txt
+	grep "443/open" $name  | awk '{print $2}' | sort -t. -k1,1n -k2.2n -k3,3n -k4,4n >> 443-https-$outname.txt
+	grep "8080/open" $name  | awk '{print $2}' | sort -t. -k1,1n -k2.2n -k3,3n -k4,4n >> 8080-https-$outname.txt
 	grep "9001/open" $name  | awk '{print $2}' | sort -t. -k1,1n -k2.2n -k3,3n -k4,4n >> 9001-$outname.txt
+	grep "3268/open" $name  | awk '{print $2}' | sort -t. -k1,1n -k2,2n -k3,3n -k4,4n >> 3268-globalcat-$outname.txt
+	grep "389/open" $name  | awk '{print $2}' | sort -t. -k1,1n -k2,2n -k3,3n -k4,4n >> 389-ldap-$outname.txt
+	grep -e "389/open" -e "636/open" -e "3268/open" -e "3269/open" $name  | awk '{print $2}' | sort -t. -k1,1n -k2,2n -k3,3n -k4,4n >> dc-hunt-test-$outname.txt
 	grep "/open" $name  | awk '{print $2}' | sort -t. -k1,1n -k2,2n -k3,3n -k4,4n >> all-$outname.txt
 	echo "--------------------------------------"
 done 
