63ae732473e1a051b6683dbbb168c916558d6438
Ecksy <38439556+Ecksy@users.noreply.github.com>
2019-04-02 15:06:04 -0500

    Add files via upload

--- batchnslookup.sh
+++ batchnslookup.sh
@@ -3,8 +3,8 @@ filename="$1"
 while read -r line
 do
     echo "***********************************************************************************************" | tee -a client-nslookup.txt
-    echo "***********************************************************************************************" | tee -a client-nslookup.txt
-    echo "" | tee -a client-nslookup.txt
+#    echo "***********************************************************************************************" | tee -a client-nslookup.txt
+#    echo "" | tee -a client-nslookup.txt
     echo "nslookup query for:" $line | tee -a client-nslookup.txt
     nslookup $line | tee -a client-nslookup.txt
     echo "" | tee -a client-nslookup.txt
