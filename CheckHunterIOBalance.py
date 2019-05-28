580b09266d1acbce4be6216f9598454f8204b712
Ecksy <38439556+Ecksy@users.noreply.github.com>
2019-05-28 12:02:33 -0500

    Add files via upload

---
+++ CheckHunterIOBalance.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python
+
+import sys
+import requests
+import datetime
+
+header = '''
+Hunter.io balance
+------------------------------------'''
+print(header)
+
+#paste in your api key
+api_key=""
+
+#get account data for API status
+request = requests.get('https://api.hunter.io/v2/account?api_key=' + api_key)
+
+#get account data in JSON form
+json_data = requests.get('https://api.hunter.io/v2/account?api_key=' + api_key).json()
+
+#print(request.status_code)
+
+#check if connectivity is up or down
+if request.status_code == 200:
+	print('Query: ' + str(request.status_code) + ' Success, API up!')
+elif request.status_code == 401:
+	print('!!!! API key invalid, check your API access, typo or you need to add your key to the script')
+	sys.exit("")
+elif request.status_code == 429:
+	print('Monthly quota reached')
+	sys.exit("")
+else:
+	print('Query: Oops, API Down! Check connectivity to Hunter.io')
+	sys.exit("")
+
+#put account data in to variables
+firstName = json_data['data']['first_name']
+lastName = json_data['data']['last_name']
+emailAddress = json_data['data']['email']
+requestsTotal = json_data['data']['calls']['available']
+requestsUsed = json_data['data']['calls']['used']
+resetDate = json_data['data']['reset_date']
+planType = json_data['data']['plan_name']
+
+#calculate days remaining before reset
+#get todays date
+today = datetime.datetime.today()
+#convert account reset date from string to datetime
+resetDatetoDatetime = datetime.datetime.strptime(resetDate, "%Y-%m-%d")
+#calculate days to reset
+daystoReset = resetDatetoDatetime - today
+#calculate remaining requests
+requestsRemaining = requestsTotal - requestsUsed
+
+#print the loot
+print("")
+print('Name:			' + firstName + " " + lastName)
+print('Account:		' + emailAddress)
+print('Account Type:		' + planType)
+print('Requests Total:		' + str(requestsTotal))
+print('Requests Used:		' + str(requestsUsed))
+print('Requests Remaining:	' + str(requestsRemaining))
+print('Reset Date:		' + resetDate)
+print('Days until reset:	' + str(daystoReset.days))
+sys.exit()
