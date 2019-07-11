bbb26023437dcaa2cba9d3ee594a86bc61c6d8c5
Ecksy <38439556+Ecksy@users.noreply.github.com>
2019-07-11 16:40:25 -0500

    Update smbShareLister.sh

--- smbShareLister.sh
+++ smbShareLister.sh
@@ -1,1 +1,13 @@
-
+#!/usr/bin/bash
+filename="$1"
+echo "Checking SMB shares: $(date)" | tee -a client-smblist.txt
+echo "" | tee -a client-smblist.txt
+while read -r line
+do
+    echo "*********************************************************************" | tee -a client-smblist.txt
+    echo "*********************************************************************" | tee -a client-smblist.txt
+    echo "" | tee -a client-smblist.txt
+    echo "SMB shares for:" $line | tee -a client-smblist.txt
+    smbclient -N -L //$line | tee -a client-smblist.txt
+    echo "" | tee -a client-smblist.txt
+done < "$filename"
