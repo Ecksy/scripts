51560ecbbd1f14d4692b5b36ae8410d65392bc53
Ecksy <38439556+Ecksy@users.noreply.github.com>
2020-05-26 16:08:37 -0500

    Update firefoxlinks-create.sh

--- firefoxlinks-create.sh
+++ firefoxlinks-create.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/bash
 filename="$1"
+lineCount=$(wc $filename | awk '{print $1}')
 
 #Comment and uncomment below for port suffix
 
@@ -7,4 +8,4 @@ filename2=$(awk '{print "http://" $1}' "$filename")
 #filename2=$(awk '{print "https://" $1}' "$filename")
 #filename2=$(awk '{print "https://" $1 ":8080"}' "$filename")
 echo "$filename2" > "${filename}-links.txt"
-echo 'Success!!! File created!'
+echo 'Success!!! File created with $lineCount links!'
