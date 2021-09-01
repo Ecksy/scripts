b4012074eaf6523c4ae700142ee36b4209a7aaa0
Ecksy <38439556+Ecksy@users.noreply.github.com>
2021-09-01 16:52:35 -0500

    Update batchmssqlinstance.sh

--- batchmssqlinstance.sh
+++ batchmssqlinstance.sh
@@ -4,7 +4,7 @@ echo "Running Impacket MSSQL Instance queries: $(date)" | tee -a $1-mssqlinstanc
 echo "" | tee -a $1-mssqlinstance.txt
 while read -r line
 do
-    echo "Querying $line for SQL Instances:"
+    echo "Querying $line for SQL Instances:" | tee -a $1-mssqlinstance.txt
     impacket-mssqlinstance $line | tee -a $1-mssqlinstance.txt
     echo "" | tee -a $1-mssqlinstance.txt
 done < "$filename"
