089d18a4a5de8c1fa870b8dbd2f307e19fbd1e4a
Ecksy <38439556+Ecksy@users.noreply.github.com>
2021-10-06 15:33:32 -0500

    Update firefoxlinks-create.sh

--- firefoxlinks-create.sh
+++ firefoxlinks-create.sh
@@ -6,6 +6,6 @@ lineCount=$(wc $filename | awk '{print $1}')
 
 filename2=$(awk '{print "http://" $1}' "$filename")
 #filename2=$(awk '{print "https://" $1}' "$filename")
-#filename2=$(awk '{print "https://" $1 ":8080"}' "$filename")
+#filename2=$(awk '{print "http://" $1 ":8080"}' "$filename")
 echo "$filename2" > "${filename}-links.txt"
 echo 'Success!!! File created with $lineCount links.'
